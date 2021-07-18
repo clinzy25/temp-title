d9137c21478b7ad9bcb5ab95099fbcba7f38203a
clinzy25 <conner.linzy25@gmail.com>
2021-07-18 17:32:18 -0400

    redux-saga setup

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,16 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import createSagaMilddleware from 'redux-saga';
 import feedReducer from './feed_reducer';
+
+const sagaMiddleware = createSagaMilddleware();
+
+const middlewares = compose(applyMiddleware(sagaMiddleware, logger));
 
 const appReducer = combineReducers({
   feedReducer,
 });
 
-const store = createStore(appReducer, applyMiddleware(logger));
+const store = createStore(appReducer, middlewares);
 
 export default store;
